d33792fa02798e4c07fe42ad2bae5a8ee2fdf882
tnur01 <145304038+tnur01@users.noreply.github.com>
2024-05-08 10:33:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -105,3 +105,68 @@ function toggleDarkMode() {
         enableDarkMode();
     }
 }
+
+// Fungsi untuk menambahkan kelas "animated" ke setiap bagian saat muncul di dalam viewport
+function checkScroll() {
+    var sections = document.querySelectorAll(".section");
+    sections.forEach(function(section) {
+        var position = section.getBoundingClientRect().top;
+        var windowHeight = window.innerHeight;
+        if (position < windowHeight * 0.75) {
+            section.classList.add("animated");
+        }
+    });
+}
+
+// Panggil fungsi checkScroll() saat halaman dimuat dan di-scroll
+window.addEventListener("load", checkScroll);
+window.addEventListener("scroll", checkScroll);
+
+// Fungsi untuk menambahkan kelas animasi saat elemen masuk ke dalam viewport
+function animateOnScroll() {
+    let lastScrollTop = 0;
+    window.addEventListener("scroll", function () {
+        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        if (currentScroll > lastScrollTop) {
+            // Scrolling down
+            document.querySelectorAll('.animated-up').forEach(element => {
+                element.classList.remove('animated-up');
+            });
+        } else {
+            // Scrolling up
+            document.querySelectorAll('.section.animated').forEach(element => {
+                if (isScrolledIntoView(element)) {
+                    element.classList.add('animated-up');
+                }
+            });
+        }
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    }, false);
+}
+
+// Fungsi untuk mengecek apakah elemen masuk ke dalam viewport
+function isScrolledIntoView(el) {
+    let rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+
+// Panggil fungsi saat halaman dimuat
+document.addEventListener("DOMContentLoaded", function() {
+    animateOnScroll();
+});
+
+// Fungsi untuk menampilkan dan menyembunyikan submenu saat tombol "Pengalaman" dihover
+function toggleSubmenu() {
+    var submenu = document.querySelector('.sub-section');
+    submenu.classList.toggle('show');
+}
+
+// Selektor untuk tombol "Pengalaman"
+var pengalamanButton = document.querySelector('.sidebar-nav ul li:nth-child(3)');
+
+// Tambahkan event listener untuk menampilkan dan menyembunyikan submenu saat tombol "Pengalaman" dihover
+pengalamanButton.addEventListener('mouseenter', toggleSubmenu);
+pengalamanButton.addEventListener('mouseleave', toggleSubmenu);
